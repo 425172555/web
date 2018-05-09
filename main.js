3fff7b56c9a8a9df3f948d6022f5266d65975298
45172555 <45172555@qq.com>
2018-05-09 18:07:46 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+// const fibona = require('./lib/fibonacci')
+const fibonacci = require('./lib/fibonacci')
+
+// console.log(fibona(10));
+console.log(fibonacci(10));
+
+
+// function isLeapYear(year) 
+// {  
+//     return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);  
+// }
+
+
+
+// console.log("2000："+isLeapYear(2000));
+
+
+
+
+
+
